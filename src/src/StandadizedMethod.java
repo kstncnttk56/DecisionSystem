7e52a3e18dfceb8ded8eb768d2b7ce93fe631174
nguyendunghy <dung.kstncnttk56@gmail.com>
2015-10-26 16:58:28 +0700

    Thêm lớp chứa hàm chuẩn hóa một mảng dữ liệu

---
+++ src/src/StandadizedMethod.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author NguyenVanDung
+ */
+public class StandadizedMethod {
+
+    public StandadizedMethod() {
+
+    }
+
+    //Viết hàm từ tên phương pháp chuẩn hóa phương pháp tại đây
+    //name là tên phương pháp
+    //value là mảng giá trị cần được chuẩn hóa
+    public static float[] standadize(int name, float[] value) {
+
+        return null;
+    }
+
+    //Viết các hàm là các phương pháp chuẩn hóa
+}
